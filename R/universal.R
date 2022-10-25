bfd96e3bfddf626996860fccb4dff338773fe03c
Karene <70621033+Karene123@users.noreply.github.com>
2022-10-25 06:48:52 -0400

    modified the code universal to include split

--- R/universal.R
+++ R/universal.R
@@ -1,5 +1,5 @@
 #' Universal conference
-#' @examples 
+#' @examples
 #' set.seed(1)
 #' pvec <- c(1/3, 1/3, 1/3)
 #' n <- 100
@@ -9,32 +9,36 @@
 #' universal(nvec = nvec)
 ##'@export
 universal <- function(nvec, log_pvalue = FALSE) {
-  ploidy <- length(nvec) - 1
-  mapply(0:ploidy, nvec, FUN = rep) |>
-    unlist() |>
-    sample(size = round(sum(nvec) / 2), replace = FALSE) |>
-    factor(levels = 0:ploidy) |>
-    table() |>
-    as.vector() ->
-    group1
-  group2 <- nvec - group1
-  
-  mle1 <- hwep::rmlike(nvec = group1)$p
-  qvec1 <- rmexact::gfreq(pvec = mle1)
-  mle2 <- hwep::rmlike(nvec = group2)$p
-  qvec2 <- rmexact::gfreq(pvec = mle2)
-  lrs1 <- dmultinom(x = group1, size = sum(group1), prob = qvec1, log = TRUE) - 
-    dmultinom(x = group1, size = sum(group1), prob = qvec2, log = TRUE)
-    
-  lrs2 <- dmultinom(x = group2, size = sum(group2), prob = qvec2, log = TRUE) -
-    dmultinom(x = group2, size = sum(group2), prob = qvec1, log = TRUE)
-  
+#"  ploidy <- length(nvec) - 1
+#'  mapply(0:ploidy, nvec, FUN = rep) |>
+#'    unlist() |>
+#'    sample(size = round(sum(nvec) / 2), replace = FALSE) |>
+#'    factor(levels = 0:ploidy) |>
+#'    table() |>
+#'    as.vector() ->
+#'    group1
+#'  group2 <- nvec - group1
+#'
+#'  mle1 <- hwep::rmlike(nvec = group1)$p
+#'  qvec1 <- rmexact::gfreq(pvec = mle1)
+#'  mle2 <- hwep::rmlike(nvec = group2)$p
+#'  qvec2 <- rmexact::gfreq(pvec = mle2)
+  qvec1 <- rmslrt(nvec = nvec, sprop = 0.5, log_p = TRUE)$q1
+  qvec2 <- rmslrt(nvec = nvec, sprop = 0.5, log_p = TRUE)$q2
+  nvec1 <- rmslrt(nvec = nvec, sprop = 0.5, log_p = TRUE)$nvec1
+  nvec2 <- rmslrt(nvec = nvec, sprop = 0.5, log_p = TRUE)$nvec2
+
+  lrs1 <- dmultinom(x = nvec1, prob = qvec1, log = TRUE) -
+    dmultinom(x = nvec1, prob = qvec2, log = TRUE)
+
+  lrs2 <- dmultinom(x = nvec2, prob = qvec2, log = TRUE) -
+    dmultinom(x = nvec2, prob = qvec1, log = TRUE)
+
   lpval <- min(log(2) - log_sum_exp(c(lrs1, lrs2)), 0)
-  
+
   if (!log_pvalue) {
     lpval <- exp(lpval)
   }
   return(lpval)
 }
-
 
